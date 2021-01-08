d908d2427105deac57ce485feb65d9e34b21389b
nezam0003 <fhameedk.nezam@gmail.com>
2021-01-08 23:25:53 +0600

    dynamic filterable item added by js

--- app.js
+++ app.js
@@ -77,3 +77,66 @@ const displaySpecialCars = ((createCars) => {
     });
 
 })(createCars);
+
+// display all cars
+const displayCars = ((createCars) => {
+    const cars = createCars.cars;
+    const inventory = document.querySelector('.inventory-container');
+    document.addEventListener('DOMContentLoaded', () => {
+        inventory.innerHTML = '';
+        let output = '';
+        cars.forEach(car => {
+            output += `<!-- single car start -->
+            <div class="col-10 mx-auto my-3 col-md-6 col-lg-4 single-car ${car.country}">
+                <div class="card car-card">
+                    <img src="${car.img}" class="card-img-top car-img" alt="german">
+                    <div class="card-body">
+                        <div class="car-info d-flex justify-content-between">
+                            <!-- first flex-child -->
+                            <div class="car-text text-uppercase">
+                                <h6 class="font-weight-bold">${car.make}</h6>
+                                <h6>${car.model}</h6>
+                            </div>
+                            <!-- Second flex-child -->
+                            <div class="car-value align-self-center py-2 px-3">
+                                $<span class="car-price">${car.price}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer text-capitalize d-flex justify-content-between">
+                        <p>
+                            <span><i class="fas fa-car"></i> ${car.type}</span>
+                        </p>
+                        <p>
+                            <span><i class="fas fa-cogs"></i> ${car.trans}</span>
+                        </p>
+                        <p>
+                            <span><i class="fas fa-gas-pump"></i> ${car.gas}</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <!-- single car end -->`;
+        });
+        inventory.innerHTML = output;
+    });
+})(createCars);
+
+// Filter cars by category
+const filterCars = (() => {
+    const filterBtn = document.querySelectorAll('.filter-btn');
+    filterBtn.forEach(btn => {
+        btn.addEventListener('click', (e) => {
+            const value = e.target.dataset.filter;
+            const singleCar = document.querySelectorAll('.single-car');
+
+            singleCar.forEach(car => {
+                if (value === 'all') {
+                    car.style.display = 'block';
+                } else {
+                    (!car.classList.contains(value)) ? car.style.display = 'none': car.style.display = 'block';
+                }
+            });
+        });
+    });
+})();
